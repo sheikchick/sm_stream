dbba8e0de0f695f045814337e4e5ff83dbc88c2f
cleo <126768211+a-cleo@users.noreply.github.com>
2023-03-11 15:57:19 +0000

    slippi js update

--- node/stats.js
+++
@@ -1,7 +0,0 @@
-const { SlippiGame } = require("@slippi/slippi-js");
-
-const game = new SlippiGame(process.argv[2]);
-
-// Get game settings – stage, characters, etc
-const stats = game.getStats();
-console.log(JSON.stringify(stats));

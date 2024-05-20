415e3edc2ebedb02cb93850a8cff462fd86f745d
a-cleo <cleofromnowon@gmail.com>
2024-05-20 22:01:51 +0100

    Fixed recording / clips, alterations to character info

--- recordLive.js
+++ recordLive.js
@@ -1,82 +1,101 @@
 const path = require("path");
 const fs = require("fs/promises");
 
+const logging = require("./logging.js");
 const { readData, INFO } = require("./data.js");
 
-let current_timestamp = "";
-let recording_status = false;
+const FILE_NOT_FOUND = "FILE NOT FOUND";
+let setStartedAt = "";
 
 exports.saveRecording = async (timecode) => {
-    if(recording_status) {
+    await rejectIfObsNotRecording();
+
+    if(setStartedAt) {
         //end recording
-        const {recordDirectory} = await obs.call('GetRecordDirectory')
-        console.log("record directory " + recordDirectory)
-        const input_filename = await getLatestRecordingFile(recordDirectory);
-        if (input_filename) {
-            const data = await readData(INFO);
-            const filename = `${data.Player1.name} vs ${data.Player2.name} - ${data.round}`.replace(/[/\\?%*:|"<>]/g, '');
-            const command = `ffmpeg -i "${input_filename}" -ss ${current_timestamp} -to ${timecode} -c copy "${filename}.mp4"\n`;
 
-            const thumb_data = `
-            ${data.Player1.name}
-            ${data.Player1.character}
-            ${data.Player1.name_dubs}
-            ${data.Player1.character_dubs}
-            ${data.Player1.score}
-            ${data.Player2.name}
-            ${data.Player2.character}
-            ${data.Player2.name_dubs}
-            ${data.Player2.character_dubs}
-            ${data.Player2.score}
-            ${data.round}
-            ${data.tournament}
-            ${data.is_doubles}
-            \n`;
+        const {recordDirectory} = await obs.call('GetRecordDirectory');
+        const [data, vod] = await Promise.all([
+            readData(INFO),
+            getLatestRecordingFile(recordDirectory)
+        ]);
 
-            await fs.appendFile(`${recordDirectory}/sets.bat`, command, "utf8");
-            await fs.appendFile(`${recordDirectory}/sets.csv`, thumb_data, "utf8");
-        }
-        current_timestamp = "";
-        recording_status = false;
+        const videoName = `${data.Player1.name} vs ${data.Player2.name} - ${data.round}`.replace(/[/\\?%*:|"<>]/g, '');
+        const command = `ffmpeg -i "${vod}" -ss ${setStartedAt} -to ${timecode} -c copy "${videoName}.mp4"\n`;
+        const batFile = path.join(recordDirectory, 'sets.bat');
+
+        await fs.appendFile(batFile, command, "utf8");
+        logging.info(`Command to extract set from VoD saved to ${batFile}`);
+        
+        setStartedAt = "";
+        if (vod === FILE_NOT_FOUND) {
+            const message = "Unable to find VoD. Command saved with placeholder filename";
+            logging.error(message)
+            throw new Error(message); // Throw simply to return a 500
+        } 
     } else {
         //start recording
-        current_timestamp = timecode;
-        recording_status = true;
+        setStartedAt = timecode;
     }
+
     return this.getRecordingStatus();
 };
 
-exports.saveClip = (timecode) => obs.call('GetRecordDirectory').then((value) => {
-    const clipDirectory = `${value.recordDirectory}/clips`;
-    return getLatestRecordingFile(clipDirectory).then((input_filename) => {
-        if (input_filename) {
-            const regex = /\d\d(?::\d\d)+/;
-            const noMillis = timecode.match(regex)?.[0].split(":") || [];
-            const seconds = (noMillis.pop() || 0) - 30;
-            const minutes = (noMillis.pop() || 0) - (seconds < 0 | 0);
-            const hours = (noMillis.pop() || 0) - (minutes < 0 | 0);
-            const ss = hours < 0
-                ? "00:00:00"
-                : `${`${hours}`.padStart(2, '0')}:${`${(60 + minutes) % 60}`.padStart(2, '0')}:${`${(60 + seconds) % 60}`.padStart(2, '0')}`;
+exports.saveClip = async (timecode) => {
+    await rejectIfObsNotRecording();
 
-            const command = `ffmpeg -i "${input_filename}" -ss ${ss} -to ${timecode} -c copy "${timecode.replaceAll(":", "-")}.mp4"\n`;
-            return fs.appendFile(`${clipDirectory}/clips.bat`, command, "utf8");
-        }
+    const recordDirectory = path.join(
+        (await obs.call('GetRecordDirectory')).recordDirectory,
+        'clips'
+    );
+    const vod = await getLatestRecordingFile(recordDirectory);
+
+    const videoName = timecode.replaceAll(":", "-");
+    const command = `ffmpeg -i "${vod}" -ss ${subtractFromTimescode(timecode)} -to ${timecode} -c copy "${videoName}.mp4"\n`;
+    const batFile = path.join(recordDirectory, 'clips.bat');
+
+    await fs.appendFile(batFile, command, "utf8");
+    logging.info(`Command to extract set from VoD saved to ${batFile}`);
+
+    if (vod === FILE_NOT_FOUND) {
+        const message = "Unable to find VoD. Command saved with placeholder filename";
+        logging.error(message)
+        throw new Error(message); // Throw simply to return a 500
+    } 
+};
+
+const subtractFromTimescode = (() => {
+    const regex = /\d\d(?::\d\d)+/;
+    const startOfVod = "00:00:00";
+    return (timecode) => {
+        const noMillis = timecode.match(regex)?.[0].split(":") || [];
+        const seconds = (noMillis.pop() || 0) - 30;
+        const minutes = (noMillis.pop() || 0) - (seconds < 0 | 0);
+        const hours = (noMillis.pop() || 0) - (minutes < 0 | 0);
+        return hours < 0
+            ? startOfVod
+            : `${`${hours}`.padStart(2, '0')}:${`${(60 + minutes) % 60}`.padStart(2, '0')}:${`${(60 + seconds) % 60}`.padStart(2, '0')}`;
+    };
+})();
+
+const rejectIfObsNotRecording = () => new Promise((resolve, reject) => {
+    obs.call('GetRecordStatus').then(({outputActive}) => {
+        outputActive
+            ? resolve()
+            : reject();
     });
 });
 
-/**
- * recording_status is a let, so we use !! to return its value rather than its reference
- * so that it cannot be modified from outwith this file.
- */ 
-exports.getRecordingStatus = () => !!recording_status;
+exports.getRecordingStatus = () => !!setStartedAt;
 
 const getLatestRecordingFile = (directory) => fs.mkdir(directory, {recursive: true})
     .then(() => fs.readdir(directory))
-    .then((files) => Promise.all(files.map(async (f) => {
-        const stat = await fs.stat(path.join(directory, f))
-        return [
-            f,
-            stat.mtimeMs
-        ]
-    })))
+    .then((files) => Promise.all(files.map(async (f) => [
+        f,
+        (await fs.stat(path.join(directory, f))).mtimeMs
+    ])))
+    .then((files) => files.reduce(
+        (acc, cur) => (cur[0].endsWith('.mkv') && cur[1] > acc[1])
+            ? cur
+            : acc
+        , [FILE_NOT_FOUND, 0]
+    )[0]);

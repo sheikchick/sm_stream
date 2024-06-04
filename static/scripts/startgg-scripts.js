e8fb2679bd15888dcfb82df765ea9020b3f03dd0
a-cleo <cleofromnowon@gmail.com>
2024-06-04 13:57:33 +0100

    Test for seeding on mobile

--- static/scripts/startgg-scripts.js
+++ static/scripts/startgg-scripts.js
@@ -29,24 +29,24 @@ function getTournamentEvents() {
 			},
 		}),
 	})
-	.then((res) => res.json())
-	.then((result) => {
-		$("#events").hide()
-		$("#phases").hide()
-		$("#phase_groups").hide()
-		$("#get_sets").hide()
-		if(result["data"]["tournament"] == null) {
-			$("#right_wrapper").css("display", "none")
-			return
-		}
-		$("#events").empty()
-		$("#events").append(new Option("Select...", 0));
-        for (let event of result["data"]["tournament"]["events"]) {
-			event_option = new Option(event["name"], event["id"]);
-			$("#events").append(event_option);
-			$("#events").show()
-		}
-	});
+		.then((res) => res.json())
+		.then((result) => {
+			$("#events").hide()
+			$("#phases").hide()
+			$("#phase_groups").hide()
+			$("#get_sets").hide()
+			if (result["data"]["tournament"] == null) {
+				$("#right_wrapper").css("display", "none")
+				return
+			}
+			$("#events").empty()
+			$("#events").append(new Option("Select...", 0));
+			for (let event of result["data"]["tournament"]["events"]) {
+				event_option = new Option(event["name"], event["id"]);
+				$("#events").append(event_option);
+				$("#events").show()
+			}
+		});
 }
 
 /* GET PHASES IN EVENT (Pools, Pro Bracket, ...) */
@@ -81,24 +81,24 @@ function getEventPhases() {
 			},
 		}),
 	})
-	.then((res) => res.json())
-	.then((result) => {
-		$("#phases").hide()
-		$("#phase_groups").hide()
-		$("#get_sets").hide()
-		if(result["data"]["event"]["phases"] == null) {
-			$("#right_wrapper").css("display", "none")
-			return
-		}
-		$("#phases").empty()
-		$("#phases").append(new Option("Select...", 0));
-        for (let phase of result["data"]["event"]["phases"]) {
-			phase_option = new Option(phase["name"], phase["id"]);
-			$("#phases").append(phase_option);
-			$("#phases").attr("tournament_slug", tournament_slug)
-			$("#phases").show()
-		}
-	});
+		.then((res) => res.json())
+		.then((result) => {
+			$("#phases").hide()
+			$("#phase_groups").hide()
+			$("#get_sets").hide()
+			if (result["data"]["event"]["phases"] == null) {
+				$("#right_wrapper").css("display", "none")
+				return
+			}
+			$("#phases").empty()
+			$("#phases").append(new Option("Select...", 0));
+			for (let phase of result["data"]["event"]["phases"]) {
+				phase_option = new Option(phase["name"], phase["id"]);
+				$("#phases").append(phase_option);
+				$("#phases").attr("tournament_slug", tournament_slug)
+				$("#phases").show()
+			}
+		});
 }
 
 /* GET PHASEGROUPS IN PHASE (Pool A1, Pool A2, ...) */
@@ -129,32 +129,32 @@ function getPhaseGroups() {
 			},
 		}),
 	})
-	.then((res) => res.json())
-	.then((result) => {
-		$("#phase_groups").hide()
-		$("#get_sets").hide()
-		if(result["data"]["phase"]["phaseGroups"] == null) {
-			$("#right_wrapper").css("display", "none")
-			return
-		}
-		$("#phase_groups").empty()
-		if (result["data"]["phase"]["phaseGroups"]["nodes"].length > 1) {
-			$("#phase_groups").append(new Option("Select...", 0));
-		}
-        for (let pg of result["data"]["phase"]["phaseGroups"]["nodes"]) {
-			pg_option = new Option(pg["displayIdentifier"], pg["id"]);
-			$("#phase_groups").append(pg_option);
-			$("#phase_groups").attr("tournament_slug", tournament_slug)
-		}
-		if (result["data"]["phase"]["phaseGroups"]["nodes"].length > 1) {
-			$("#phase_groups").show()
-		} else {
-			//for overloading - select the only available option and then hide the element for clarity
-			$("#phase_groups").val($("#phase_groups option:first").val());
+		.then((res) => res.json())
+		.then((result) => {
 			$("#phase_groups").hide()
-			$("#get_sets").show()
-		}
-	});
+			$("#get_sets").hide()
+			if (result["data"]["phase"]["phaseGroups"] == null) {
+				$("#right_wrapper").css("display", "none")
+				return
+			}
+			$("#phase_groups").empty()
+			if (result["data"]["phase"]["phaseGroups"]["nodes"].length > 1) {
+				$("#phase_groups").append(new Option("Select...", 0));
+			}
+			for (let pg of result["data"]["phase"]["phaseGroups"]["nodes"]) {
+				pg_option = new Option(pg["displayIdentifier"], pg["id"]);
+				$("#phase_groups").append(pg_option);
+				$("#phase_groups").attr("tournament_slug", tournament_slug)
+			}
+			if (result["data"]["phase"]["phaseGroups"]["nodes"].length > 1) {
+				$("#phase_groups").show()
+			} else {
+				//for overloading - select the only available option and then hide the element for clarity
+				$("#phase_groups").val($("#phase_groups option:first").val());
+				$("#phase_groups").hide()
+				$("#get_sets").show()
+			}
+		});
 }
 
 
@@ -212,110 +212,110 @@ function getSets() {
 			},
 		}),
 	})
-	.then((res) => res.json())
-	.then((result) => {
-		sets = []
-		phase_group = result["data"]["phaseGroup"]
-		for (let set of phase_group["sets"]["nodes"]) {
-			valid = true
-			for (let entrant of set["slots"]) {
-				if (!(entrant["entrant"])) {
-					valid = false
-				}
-			}
-			if (valid) {
-                //match details
-				match_id = set["id"]
-				if(phase_group["bracketType"] == "ROUND_ROBIN") {
-					match_round = phase_group["phase"]["name"] + " " + phase_group["displayIdentifier"]
-				} else {
-					match_round = set["fullRoundText"]
-				}
-                //players
-				team1 = set["slots"][0]
-				team2 = set["slots"][1]
-				//player 1
-                p1_entrant = team1["entrant"]["id"]
-				p1_user_id = ""
-				p1_pronouns = ""
-				if (team1["entrant"]["participants"][0]["user"] != null) {
-					p1_user_id = team1["entrant"]["participants"][0]["user"]["discriminator"]
-					p1_pronouns = team1["entrant"]["participants"][0]["user"]["genderPronoun"]
-				}
-				p1_name = team1["entrant"]["participants"][0]["gamerTag"]
-				//player 1 doubles
-				p1_doubles_user_id = ""
-				p1_doubles_pronouns = ""
-				p1_doubles_name = ""
-				if(team1["entrant"]["participants"].length > 1) {
-					if (team1["entrant"]["participants"][1]["user"] != null) {
-						p1_doubles_user_id = team1["entrant"]["participants"][1]["user"]["discriminator"]
-						p1_doubles_pronouns = team1["entrant"]["participants"][1]["user"]["genderPronoun"]
+		.then((res) => res.json())
+		.then((result) => {
+			sets = []
+			phase_group = result["data"]["phaseGroup"]
+			for (let set of phase_group["sets"]["nodes"]) {
+				valid = true
+				for (let entrant of set["slots"]) {
+					if (!(entrant["entrant"])) {
+						valid = false
 					}
-					p1_doubles_name = team1["entrant"]["participants"][1]["gamerTag"]
 				}
+				if (valid) {
+					//match details
+					match_id = set["id"]
+					if (phase_group["bracketType"] == "ROUND_ROBIN") {
+						match_round = phase_group["phase"]["name"] + " " + phase_group["displayIdentifier"]
+					} else {
+						match_round = set["fullRoundText"]
+					}
+					//players
+					team1 = set["slots"][0]
+					team2 = set["slots"][1]
+					//player 1
+					p1_entrant = team1["entrant"]["id"]
+					p1_user_id = ""
+					p1_pronouns = ""
+					if (team1["entrant"]["participants"][0]["user"] != null) {
+						p1_user_id = team1["entrant"]["participants"][0]["user"]["discriminator"]
+						p1_pronouns = team1["entrant"]["participants"][0]["user"]["genderPronoun"]
+					}
+					p1_name = team1["entrant"]["participants"][0]["gamerTag"]
+					//player 1 doubles
+					p1_doubles_user_id = ""
+					p1_doubles_pronouns = ""
+					p1_doubles_name = ""
+					if (team1["entrant"]["participants"].length > 1) {
+						if (team1["entrant"]["participants"][1]["user"] != null) {
+							p1_doubles_user_id = team1["entrant"]["participants"][1]["user"]["discriminator"]
+							p1_doubles_pronouns = team1["entrant"]["participants"][1]["user"]["genderPronoun"]
+						}
+						p1_doubles_name = team1["entrant"]["participants"][1]["gamerTag"]
+					}
 
-				//player 2
-                p2_entrant = team2["entrant"]["id"]
-				p2_user_id = ""
-				p2_pronouns = ""
-				if (team2["entrant"]["participants"][0]["user"] != null) {
-					p2_user_id = team2["entrant"]["participants"][0]["user"]["discriminator"]
-					p2_pronouns = team2["entrant"]["participants"][0]["user"]["genderPronoun"]
-				}
-				p2_name = team2["entrant"]["participants"][0]["gamerTag"]
-				//player 2 doubles
-				p2_doubles_user_id = ""
-				p2_doubles_pronouns = ""
-				p2_doubles_name = ""
-				if(team2["entrant"]["participants"].length > 1) {
-					if (team2["entrant"]["participants"][1]["user"] != null) {
-						p2_doubles_user_id = team2["entrant"]["participants"][1]["user"]["discriminator"]
-						p2_doubles_pronouns = team2["entrant"]["participants"][1]["user"]["genderPronoun"]
+					//player 2
+					p2_entrant = team2["entrant"]["id"]
+					p2_user_id = ""
+					p2_pronouns = ""
+					if (team2["entrant"]["participants"][0]["user"] != null) {
+						p2_user_id = team2["entrant"]["participants"][0]["user"]["discriminator"]
+						p2_pronouns = team2["entrant"]["participants"][0]["user"]["genderPronoun"]
 					}
-					p2_doubles_name = team2["entrant"]["participants"][1]["gamerTag"]
+					p2_name = team2["entrant"]["participants"][0]["gamerTag"]
+					//player 2 doubles
+					p2_doubles_user_id = ""
+					p2_doubles_pronouns = ""
+					p2_doubles_name = ""
+					if (team2["entrant"]["participants"].length > 1) {
+						if (team2["entrant"]["participants"][1]["user"] != null) {
+							p2_doubles_user_id = team2["entrant"]["participants"][1]["user"]["discriminator"]
+							p2_doubles_pronouns = team2["entrant"]["participants"][1]["user"]["genderPronoun"]
+						}
+						p2_doubles_name = team2["entrant"]["participants"][1]["gamerTag"]
+					}
+
+					match_data = {
+						"id": set["id"],
+						"round": match_round,
+						"player1": {
+							"entrant_id": p1_entrant,
+							"data": [
+								{
+									"id": p1_user_id,
+									"name": p1_name,
+									"pronouns": p1_pronouns
+								},
+								{
+									"id": p1_doubles_user_id,
+									"name": p1_doubles_name,
+									"pronouns": p1_doubles_pronouns
+								}
+							]
+						},
+						"player2": {
+							"entrant_id": p2_entrant,
+							"data": [
+								{
+									"id": p2_user_id,
+									"name": p2_name,
+									"pronouns": p2_pronouns
+								},
+								{
+									"id": p2_doubles_user_id,
+									"name": p2_doubles_name,
+									"pronouns": p2_doubles_pronouns
+								}
+							]
+						}
+					}
+					sets.push(match_data)
 				}
-				
-				match_data = {
-					"id": set["id"],
-					"round": match_round,
-					"player1": {
-                        "entrant_id" : p1_entrant,
-                        "data" : [
-                            {
-                            "id": p1_user_id,
-						    "name": p1_name,
-						    "pronouns": p1_pronouns
-                            },
-                            {
-                                "id": p1_doubles_user_id,
-                                "name": p1_doubles_name,
-                                "pronouns": p1_doubles_pronouns
-                            }
-                        ]
-                    },
-                    "player2": {
-                        "entrant_id" : p2_entrant,
-                        "data": [
-                            {
-                                "id": p2_user_id,
-                                "name": p2_name,
-                                "pronouns": p2_pronouns
-                            },
-                            {
-                                "id": p2_doubles_user_id,
-                                "name": p2_doubles_name,
-                                "pronouns": p2_doubles_pronouns
-                            }
-                        ]
-                    }
-				}
-				sets.push(match_data)
 			}
-		}
-		set_page = 0;
-		showSets(true);
-	});
+			set_page = 0;
+			showSets(true);
+		});
 }
 
 
@@ -342,6 +342,10 @@ function submitSet(setId, winnerId, gameData) {
 			},
 		}),
 	})
-	.then((res) => res.json())
-	.then((result) => {})
+		.then((res) => res.json())
+		.then((result) => { })
+}
+
+function showGetSeeds() {
+	$('#getSeeds').show();
 }
